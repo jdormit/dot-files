af7637450f683199a1377e640aa45bcc791b7150
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2016-08-16 15:54:22 -0600

    Add sudo-open-file command to Emacs

--- init.el
+++ init.el
@@ -163,5 +163,11 @@ prompt the user for a coding system."
               "$SHELL -cl \"printf %s \\\"\\\$PATH\\\"\"")))
         (setenv "PATH" path)))
 
+(defun sudo-find-file (file-name)
+  "Like find file, but opens the file as root."
+  (interactive "FSudo Find File: ")
+  (let ((tramp-file-name (concat "/sudo::" (expand-file-name file-name))))
+    (find-file tramp-file-name)))
+
 (global-wakatime-mode)
 
