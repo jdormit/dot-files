0397d37d658b4e08694007e76e438a1ffa744e5a
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2017-02-16 14:18:06 -0700

    Remove nvpy from simplenote syncing

--- bin/notes-sync.py
+++ bin/notes-sync.py
@@ -13,17 +13,6 @@ import re
 # Regex to identify the note title - the first line with non-whitespace
 note_title_re = re.compile('\s*(.*)\n?')
 
-def note_markdown(n):
-    """True if Simplenote has flagged the note as a markdown file"""
-    asystags = n.get('systemtags', 0)
-    # no systemtag at all
-    if not asystags:
-        return 0
-    if 'markdown' in asystags:
-        return 1
-    else:
-        return 0
-
 def get_note_title_file(note):
     """Return the filename corresponding to the note"""
     mo = note_title_re.match(note.get('content', ''))
@@ -38,46 +27,59 @@ def get_note_title_file(note):
             fn = unicode(fn, 'utf-8')
         else:
             fn = unicode(fn)
-            
-        if note_markdown(note):
-            fn += '.mkdn'
-        else:
-            fn += '.txt'
 
+        fn += '.txt'
         return fn
     else:
         return ''
     
-nvpyConfig = ConfigParser()
-nvpyConfig.read(path.join(environ['HOME'], '.nvpy.cfg'))
+notesConfig = ConfigParser()
+notesConfig.read(path.join(environ['HOME'], '.notes.cfg'))
 
-sn_username = nvpyConfig.get('nvpy', 'sn_username')
-sn_password = nvpyConfig.get('nvpy', 'sn_password')
-notes_dir = nvpyConfig.get('nvpy', 'txt_path')
+sn_username = notesConfig.get('notes', 'sn_username')
+sn_password = notesConfig.get('notes', 'sn_password')
+notes_dir = notesConfig.get('notes', 'notes_dir')
 
 simplenote = Simplenote(sn_username, sn_password)
 
 # Get notes list
 notes_without_content, _ = simplenote.get_note_list()
 
-# Populate the notes
-notes = [simplenote.get_note(note['key'])[0] for note in notes_without_content]
+# Populate the notes that are not deleted
+notes_with_content = [simplenote.get_note(note['key'])[0] for note in notes_without_content if not note['deleted']]
+remote_note_files = { get_note_title_file(note): note for note in notes_with_content }
 
-# Which notes do I have locally that are not in the notes list?
-remote_note_files = {get_note_title_file(note) : note for note in notes if get_note_title_file(note) != ''}
 local_note_files = listdir(unicode(notes_dir, 'utf-8'))
 
+# Upload missing local files to the remote
 for local_note_file in local_note_files:
     if local_note_file in remote_note_files.keys():
-        # Update the remote note with the contents of the local note
-        with open(path.join(notes_dir, local_note_file), 'r') as f:
-            content = f.read()
-            remote_note = remote_note_files[local_note_file]
-            remote_note['content'] = content
-            simplenote.update_note(remote_note)
+        # Update the remote note with the contents of the local note,
+        # if the local note has been updated after the remote note
+        if path.getmtime(path.join(notes_dir, local_note_file)) > float(remote_note_files[local_note_file]['modifydate']):
+            with open(path.join(notes_dir, local_note_file), 'r') as f:
+                content = f.read()
+                remote_note = remote_note_files[local_note_file]
+                remote_note['content'] = content
+                remote_note['modifydate'] = path.getmtime(path.join(notes_dir, local_note_file))
+                simplenote.update_note(remote_note)
     else:
         # Or create a new remote note from the the local note
         with open(path.join(notes_dir, local_note_file), 'r') as f:
             content = f.read()
             local_note = {'content': content}
             simplenote.add_note(local_note)
+
+# Download missing remote files to the local store
+for remote_file_name, remote_note in remote_note_files.iteritems():
+    if remote_file_name not in local_note_files:
+        # Save the contents of the remote note to a local files
+        with open(path.join(notes_dir, remote_file_name), 'w') as f:
+            f.write(remote_note['content'])
+    else:
+        # Update the local file with the contents of the remote note,
+        # if the remote note has been updated after the local note
+        # and the remote note is not marked as deleted
+        if float(remote_note['modifydate']) > path.getmtime(path.join(notes_dir, remote_file_name)):
+            with open(path.join(notes_dir, remote_file_name), 'w') as f:
+                f.write(remote_note['content'])
